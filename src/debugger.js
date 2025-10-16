046463311c247c551eac0d4dd1676f22af506fc9
modify <2645929971@qq.com>
2025-10-16 17:11:54 +0800

    feat(bundle): 实现代码打包生成功能
    - 为statement的_included属性添加writable配置
    - 引入MagicString库用于代码生成- 实现generate方法生成最终打包代码
    - 更新fetchModule方法参数命名和返回逻辑
    - 添加文件写入功能，将打包结果输出到指定路径
    - 调整调试脚本输出路径
    - 在package.json中指定node版本要求

--- src/debugger.js
+++ src/debugger.js
@@ -2,6 +2,6 @@ const path = require('path');
 const rollup = require('../lib/rollup');
 
 const entry = path.resolve(__dirname, 'main.js');
-const output = path.resolve(__dirname, 'dist/bundle.js');
+const output = path.resolve(__dirname, '../dist/bundle.js');
 
 rollup(entry, output);
