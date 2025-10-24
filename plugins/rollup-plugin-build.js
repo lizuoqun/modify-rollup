b156ae94c5741621085900459c587f3b54838e59
modify <2645929971@qq.com>
2025-10-24 15:58:02 +0800

    feat(rollup): 添加构建插件并配置 rollup
    - 在 package.json 中添加 "type": "module" 以支持 ES 模块
    - 创建 rollup-plugin-build.js 插件文件，包含构建生命周期钩子
    - 在 rollup.config.js 中引入并配置新创建的构建插件
    - 插件实现了 options、buildStart、resolveId、load、transform 和 buildEnd 钩子函数
    - 添加了基础的插件结构和版本信息

---
+++ plugins/rollup-plugin-build.js
@@ -0,0 +1,30 @@
+/**
+ * 构建插件
+ * */
+function build() {
+
+  return {
+    name: 'rollup-plugin-build',
+    version: '0.0.1',
+    options() {
+      console.log('options');
+    },
+    buildStart() {
+      console.log('buildStart');
+    },
+    async resolveId(source, importer) {
+      console.log('resolveId', source, importer);
+    },
+    async load(id) {
+      console.log('load', id);
+    },
+    async transform(code, id) {
+      console.log('transform', code, id);
+    },
+    buildEnd() {
+      console.log('buildEnd');
+    }
+  };
+}
+
+export default build;
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import build from './plugins/rollup-plugin-build.js';
+
 export default {
   input: './src/main.js',
   output: {
     file: './dist/rollup-bundle.js',
     format: 'cjs'
-  }
+  },
+  plugins: [
+    build()
+  ]
 };
